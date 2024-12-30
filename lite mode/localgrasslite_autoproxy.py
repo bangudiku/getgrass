27bc41149b26b086e50255c32a6944c2119ca240
Dwi Putra <66292964+bangudiku@users.noreply.github.com>
2024-12-30 14:35:47 +0700

    Update localgrasslite_autoproxy.py
    work only extension

--- lite mode/localgrasslite_autoproxy.py
+++ lite mode/localgrasslite_autoproxy.py
@@ -4,88 +4,163 @@ import ssl
 import json
 import time
 import uuid
-import requests
-import shutil
-from loguru import logger
+import base64
+import aiohttp
+from datetime import datetime
+from colorama import init, Fore, Style
 from websockets_proxy import Proxy, proxy_connect
-from fake_useragent import UserAgent
 
-async def connect_to_wss(socks5_proxy, user_id):
-    user_agent = UserAgent(os=['windows', 'macos', 'linux'], browsers='chrome')
-    random_user_agent = user_agent.random
-    device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
-    logger.info(device_id)
-    while True:
-        try:
-            await asyncio.sleep(random.randint(1, 10) / 10)
-            custom_headers = {
-                "User-Agent": random_user_agent,
-                "Origin": "chrome-extension://ilehaonighjijnmpnagapkhpcdbhclfg"
-            }
-            ssl_context = ssl.create_default_context()
-            ssl_context.check_hostname = False
-            ssl_context.verify_mode = ssl.CERT_NONE
-            urilist = ["wss://proxy2.wynd.network:4444/","wss://proxy2.wynd.network:4650/"]
-            uri = random.choice(urilist)
-            server_hostname = "proxy2.wynd.network"
-            proxy = Proxy.from_url(socks5_proxy)
-            async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
-                                     extra_headers=custom_headers) as websocket:
-                async def send_ping():
-                    while True:
-                        send_message = json.dumps(
-                            {"id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}})
-                        logger.debug(send_message)
-                        await websocket.send(send_message)
-                        await asyncio.sleep(5)
+init(autoreset=True)
 
-                await asyncio.sleep(1)
-                asyncio.create_task(send_ping())
+BANNER = """
+_________ ____________________                            
+__  ____/______  /__  ____/____________ _______________
+_  / __ _  _ \\  __/  / __ __  ___/  __ `/_  ___/_  ___/
+/ /_/ / /  __/ /_ / /_/ / _  /   / /_/ /_(__  )_(__  ) 
+\\____/  \\___/\\__/ \\____/  /_/    \\__,_/ /____/ /____/  
+"""
 
+EDGE_USERAGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.2365.57",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.2365.52",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.2365.46",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.2277.128",
+]
+
+HTTP_STATUS_CODES = {
+    200: "OK",
+    201: "Created", 
+    202: "Accepted",
+    204: "No Content",
+    400: "Bad Request",
+    401: "Unauthorized",
+    403: "Forbidden", 
+    404: "Not Found",
+    500: "Internal Server Error",
+    502: "Bad Gateway",
+    503: "Service Unavailable",
+    504: "Gateway Timeout"
+}
+
+def colorful_log(proxy, device_id, message_type, message_content, is_sent=False, mode=None):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    color = Fore.GREEN if is_sent else Fore.BLUE
+    action_color = Fore.YELLOW
+    mode_color = Fore.LIGHTYELLOW_EX
+    log_message = (
+        f"{Fore.WHITE}[{timestamp}] "
+        f"{Fore.MAGENTA}[Proxy: {proxy}] "
+        f"{Fore.CYAN}[Device ID: {device_id}] "
+        f"{action_color}[{message_type}] "
+        f"{color}{message_content} "
+        f"{mode_color}[{mode}]"
+    )
+    print(log_message)
+
+async def fetch_proxies():
+    url = "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=protocolipport&format=text"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            proxies = await response.text()
+            return proxies.splitlines()
+
+async def connect_to_wss(socks5_proxy, user_id, mode):
+    device_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, socks5_proxy))
+    random_user_agent = random.choice(EDGE_USERAGENTS)
+    
+    colorful_log(
+        proxy=socks5_proxy,
+        device_id=device_id,
+        message_type="INITIALIZATION",
+        message_content=f"User Agent: {random_user_agent}",
+        mode=mode
+    )
+
+    try:
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE
+        
+        uri_list = [
+            "wss://proxy2.wynd.network:4444/",
+            "wss://proxy2.wynd.network:4650/",
+        ]
+        uri = random.choice(uri_list)
+        server_hostname = "proxy.wynd.network"
+        proxy = Proxy.from_url(socks5_proxy)
+        
+        async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname) as websocket:
+            async def send_ping():
                 while True:
-                    response = await websocket.recv()
-                    message = json.loads(response)
-                    logger.info(message)
-                    if message.get("action") == "AUTH":
-                        auth_response = {
-                            "id": message["id"],
-                            "origin_action": "AUTH",
-                            "result": {
-                                "browser_id": device_id,
-                                "user_id": user_id,
-                                "user_agent": custom_headers['User-Agent'],
-                                "timestamp": int(time.time()),
-                                "device_type": "extension",
-                                "version": "4.26.2",
-                                "extension_id": "ilehaonighjijnmpnagapkhpcdbhclfg"
-                            }
-                        }
-                        logger.debug(auth_response)
-                        await websocket.send(json.dumps(auth_response))
-
-                    elif message.get("action") == "PONG":
-                        pong_response = {"id": message["id"], "origin_action": "PONG"}
-                        logger.debug(pong_response)
-                        await websocket.send(json.dumps(pong_response))
-        except Exception as e:
-            logger.error(e)
-            logger.error(socks5_proxy)
+                    ping_message = json.dumps(
+                        {"id": str(uuid.uuid4()), "action": "PING", "timestamp": int(time.time())}
+                    )
+                    await websocket.send(ping_message)
+                    colorful_log(
+                        proxy=socks5_proxy,
+                        device_id=device_id,
+                        message_type="SENDING PING",
+                        message_content="Ping sent",
+                        is_sent=True,
+                        mode=mode
+                    )
+                    await asyncio.sleep(5)
+            
+            asyncio.create_task(send_ping())
+            
+            while True:
+                response = await websocket.recv()
+                message = json.loads(response)
+                
+                if message.get("action") == "PONG":
+                    colorful_log(
+                        proxy=socks5_proxy,
+                        device_id=device_id,
+                        message_type="PING SUCCESS",
+                        message_content="Pong received",
+                        mode=mode
+                    )
+                else:
+                    colorful_log(
+                        proxy=socks5_proxy,
+                        device_id=device_id,
+                        message_type="RECEIVED",
+                        message_content=json.dumps(message),
+                        mode=mode
+                    )
+    except Exception as e:
+        colorful_log(
+            proxy=socks5_proxy,
+            device_id=device_id,
+            message_type="ERROR",
+            message_content=str(e),
+            mode=mode
+        )
+        await asyncio.sleep(5)
 
 async def main():
-    #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
-    _user_id = input('Please Enter your user ID: ')
-    #put the proxy in a file in the format socks5://username:password@ip:port or socks5://ip:port
-    r = requests.get("https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=protocolipport&format=text", stream=True)
-    if r.status_code == 200:
-       with open('auto_proxies.txt', 'wb') as f:
-           for chunk in r:
-               f.write(chunk)
-       with open('auto_proxies.txt', 'r') as file:
-               auto_proxy_list = file.read().splitlines()
-
-    tasks = [asyncio.ensure_future(connect_to_wss(i, _user_id)) for i in auto_proxy_list]
+    print(f"{Fore.CYAN}{BANNER}{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}IM-Hanzou | GetGrass Crooter V2{Style.RESET_ALL}")
+    
+    print(f"{Fore.GREEN}Select Mode:{Style.RESET_ALL}")
+    print("1. Extension Mode")
+    print("2. Desktop Mode")
+    
+    while True:
+        mode_choice = input("Enter your choice (1/2): ").strip()
+        if mode_choice in ['1', '2']:
+            break
+        print(f"{Fore.RED}Invalid choice. Please enter 1 or 2.{Style.RESET_ALL}")
+    
+    mode = "extension" if mode_choice == "1" else "desktop"
+    print(f"{Fore.GREEN}Selected mode: {mode}{Style.RESET_ALL}")
+    
+    _user_id = input("Enter user ID: ").strip()
+    proxies = await fetch_proxies()
+    
+    print(f"{Fore.YELLOW}Total Proxies: {len(proxies)}{Style.RESET_ALL}")
+    tasks = [asyncio.ensure_future(connect_to_wss(proxy, _user_id, mode)) for proxy in proxies]
     await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
-    #letsgo
     asyncio.run(main())
